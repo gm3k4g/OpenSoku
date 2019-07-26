dd59090a13ace84504b6714e9aa6b963afc776cd
g_oddball <driftolost@gmail.com>
2019-07-26 23:57:41 +0300

    Added yuyuko

--- src/chars/yuyuko.h
+++ src/chars/yuyuko.h
@@ -5,15 +5,18 @@ class char_yuyuko: public char_c
 {
 public:
 
+	int32_t field_890;
 
 	char_yuyuko(inp_ab *func,uint8_t pal = 0);
 
+	void sub_6E28C0();
+
 	void func10(); //Number of variables detected: 1882
-	//void func20();
+	void func20();
 	//void func24();
 	void init_vars();
-	//void set_seq_params();
-	//c_bullet *new_bullet();
+	void set_seq_params();
+	c_bullet *new_bullet();
 };
 
 #endif // YUYUKO_CPP_INCLUDED
---
+++ src/chars/yuyuko_bullets.h
@@ -0,0 +1,12 @@
+#ifndef YUYUKO_BULL_CPP_INCLUDED
+#define YUYUKO_BULL_CPP_INCLUDED
+
+class yuyuko_bullets: public c_bullet
+{
+public:
+
+	void func10();
+	void set_seq_params(); //func15?
+};
+
+#endif // CIRNO_CPP_INCLUDED
